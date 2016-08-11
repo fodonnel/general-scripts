afa990de5c6f9f8b009bf02fcb9170651185e0f5
Fergal O'Donnell <fergal.odonnell@gmail.com>
2016-08-11 13:15:56 +0000

    Added explorer options

--- ConfigureWin10.ps1
+++ ConfigureWin10.ps1
@@ -2,7 +2,9 @@
 function Run-Script() {
     Remove-WinApps
     Install-ChocoPackages
+    Set-ExplorerOptions
 }
+
 
 function Install-ChocoPackages() {
     $packages = @(
@@ -18,6 +20,7 @@ function Install-ChocoPackages() {
         & choco install $package
     }
 }
+
 
 function Remove-WinApps() {
     $apps = @(
@@ -39,6 +42,17 @@ function Remove-WinApps() {
     foreach ($app in $apps) {
         get-appxpackage $app | remove-appxpackage
     }
+}
+
+
+function Set-ExplorerOptions() {
+    Push-Location
+    Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
+    Set-ItemProperty . Hidden 1
+    Set-ItemProperty . HideFileExt 0
+    Set-ItemProperty . ShowSuperHidden 1
+    Set-ItemProperty . DontPrettyPath 0
+    Pop-Location
 }
 
 Run-Script
